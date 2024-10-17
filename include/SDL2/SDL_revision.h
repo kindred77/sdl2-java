da3a96e02b3ba1f2916f141e7a80d32fda91a310
unknown <kindred77>
2024-10-17 16:07:43 +0800

    add SDL headers and version

---
+++ include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.30.8-13-g7107e0231 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.30.8-13-g7107e0231"
+#endif
